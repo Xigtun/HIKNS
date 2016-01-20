f07ecba9737c65b6c8f38aced762bbf884ee4760
cysu <cysu@cysudemac-mini.local>
2016-01-20 10:09:45 +0800

    Initial Commit

---
+++ HIKNS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HIKNS
+//
+//  Created by cysu on 16/1/20.
+//  Copyright © 2016年 cysu1077.ns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
