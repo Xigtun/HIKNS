faad4bb281ea2eced09add6879bf66a6c90203db
Xigtun <cysummer9@gmail.com>
2016-02-16 08:05:30 +0800

    Add Ask Reply Tab

---
+++ HIKNS/HNReplyViewController.h
@@ -0,0 +1,13 @@
+//
+//  HNReplyViewController.h
+//  HIKNS
+//
+//  Created by cysu on 16/2/15.
+//  Copyright © 2016年 cysu1077.ns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNReplyViewController : UIViewController
+
+@end
---
+++ HIKNS/HNReplyViewController.m
@@ -0,0 +1,39 @@
+//
+//  HNReplyViewController.m
+//  HIKNS
+//
+//  Created by cysu on 16/2/15.
+//  Copyright © 2016年 cysu1077.ns. All rights reserved.
+//
+
+#import "HNReplyViewController.h"
+
+@interface HNReplyViewController ()
+
+@end
+
+@implementation HNReplyViewController
+static NSString *const kCellIdentifier = @"HNReplyViewCell";
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.title = @"All Reply";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
